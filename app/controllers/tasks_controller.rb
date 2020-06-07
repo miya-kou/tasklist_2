0a47540485f1fc202a8ebc19a75ae8fe4671156c
kohei.miyamoto <ponpokopomu@gmail.com>
2020-06-07 13:31:53 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-   before_action :set_task,only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in, only: [:index, :show]
   def index
     @tasks = Task.where(user_id: session[:user_id])
   end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 20 }
+  validates :status, presence: true, length: { maximum: 20 }
 end
