bae60c6e34d202f4a79ea09269ae5d52f32659a2
kohei.miyamoto <ponpokopomu@gmail.com>
2020-06-12 12:41:08 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show,:new,:create,:destroy]
+  before_action :correct_user, only: [:show,:edit,:update,:destroy]
   
   def index
     @tasks = Task.where(user_id: session[:user_id])
